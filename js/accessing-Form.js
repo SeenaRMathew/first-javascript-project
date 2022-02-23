9a3adfdf0b12b053bd9193674f9377f364b79a62
SeenaRMathew <seena.mathew2014@gmail.com>
2022-02-23 17:46:06 -0500

    latest update

--- js/accessing-Form.js
+++ js/accessing-Form.js
@@ -1,10 +1,14 @@
 console.log('Eventually shows from data here');
 
 
-let button =document.querySelector('#get-name-button');
-button.addEventListener('click',function()
-{
-  console.log('button clicked');
-  let input=document.querySelector('#your-name');
-console.log(`hello, ${input.value}`);
+// Add an event listener for the button
+let button = document.querySelector('#get-name-button');
+button.addEventListener('click', function () {
+  console.log('You clicked on the button');
+
+  // Get the text content of the form field
+  let input = document.querySelector('#your-name');
+
+  // Log to the console
+  console.log(`Hello, ${input.value}!`);
 });
